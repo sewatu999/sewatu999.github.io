6b1662b708c6c74dd1e5a608bd78503562e9e3d3
Abdul James <abduljames@Abduls-iMac.local>
2020-03-22 14:21:53 -0400

    create menu button,got button to react

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,10 +1,11 @@
-const menuBtn = document.querySelector('.btn-menu');
-const menu = document.querySelector('.menu-btn');
+const menuBtn = document.querySelector('.menu-btn');
+const menu = document.querySelector('.menu');
 const menuNav = document.querySelector('.menu-nav');
 const menuBranding = document.querySelector('.menu-branding');
 const navItems = document.querySelectorAll('.nav-item');
 
 let showMenu = false;
+// let toggle = toggleMenu();
 
 menuBtn.addEventListener('click', toggleMenu);
 
